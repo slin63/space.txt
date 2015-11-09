b195720f357709868cc80031c6370c23bfbd8afe
slin63 <slin63@illinois.edu>
2015-11-09 14:44:34 -0600

    coordinate plane populating finished, player interaction with objects in progress

---
+++ game_objects.py
@@ -0,0 +1,112 @@
+# Handles objects (including the player)
+from math import sqrt, atan2, degrees
+
+class C(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def d_pos(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def distance(self, other):
+        dx = other.get_x() - self.x
+        dy = other.get_y() - self.y
+        return sqrt(dx**2 + dy**2)
+
+    def theta(self, other):
+        dx = other.get_x() - self.x
+        dy = other.get_y() - self.y
+        return degrees(atan2(dy, dx))
+
+    def cardinality(self, other):
+        theta = self.theta(other)
+
+        if theta <= 22.5:
+            direction = "E"
+        if 22.5 < theta <= 67.5:
+            direction = "NE"
+        if 67.5 < theta <= 112.5:
+            direction = "N"
+        if 112.5 < theta <= 157.5:
+            direction = "NW"
+        if 157.5 < theta <= 202.5:
+            direction = "W"
+        if 202.5 < theta <= 247.5:
+            direction = "SW"
+        if 247.5 < theta <= 292.5:
+            direction = "S"
+        if 292.5 < theta <= 337.5:
+            direction = "SE"
+        if 337.5 < theta:
+            direction = "E"
+        return direction
+
+    def __str__(self):
+        return str((self.x, self.y))
+
+    def __eq__(self, other):
+        return ((self.get_x(), self.get_y()),) == ((other.get_x(), other.get_y()),)
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
+
+class Player(object):
+    def __init__(self, name, health, inventory=None, position=None):
+        self.name = name
+        self.health = health
+        self.inventory = inventory
+        self.position = position
+
+    def get_position(self):
+        return self.position
+
+    def change_position(self, dx, dy):
+        return self.position.d_pos(dx, dy)
+
+    def get_surroundings(self, vision, objects):
+        """
+        :param vision: Integer representing the maximum distance to which the player may "see"
+        :param objects: Dictionary containing objects and their coordinates. Given by Map.get_objects.
+        """
+        in_range = ()
+        pos = self.position
+        for e in objects.keys():
+            d = pos.distance(e)
+            if d <= vision:
+                in_range += ((objects[e], d, {pos.theta(e):pos.cardinality(e)}), )
+
+        return in_range
+
+    def __str__(self):
+        vals = [self.name, self.health, self.inventory, self.position]
+        stringrep = ""
+        for e in vals:
+            stringrep += str(e) + ", "
+
+        return stringrep[:-2]
+
+    def __repr__(self):
+        return str((self.x, self.y))
+#
+obj_test = {
+    C(0, 0): 'STAR322B', C(4, 15): 'WRECK', C(12, 5): 'SAT', C(6, 15): 'SAT',
+    C(12, 14): 'STAR353A', C(2, 18): 'WRECK', C(14, 9): 'DEBRIS', C(10, 2): 'WRECK', C(13, 1): 'DEBRIS', C(12, 7): 'DEBRIS'
+}
+p = Player("p", 100, position=C(0,0))
+# print p.get_position()
+print p.get_surroundings(15, obj_test)
+
+# a = C(0,0)
+# b = C(3,4)
+# print("Distance = %s" % a.distance(b))
+# print("Theta = %s" % a.theta(b))
+# print("Cardinality = %s" % a.cardinality(b))
--- movement.py
+++ movement.py
@@ -1,19 +1,21 @@
 # Handles player and scene locations
 from random import randint, choice
+from game_objects import Player
+from math import sqrt, atan2, degrees
 
 
 class Map(object):
     """Object representing both the overworld map and the superclass of unique locations
     within the overworld."""
-    def __init__(self, size, objects, density=0.05):
+    def __init__(self, size, objects, density=0.05, player=None):
         """Size is an intenger value representing the length of the square overworld.
            Objects is a list of possible objects that will spawn in this instance.
            Density represents the decimal probability of an object spawning in a given coordinate"""
         self.size = size
         self.objects = objects
         self.density = density
+        self.player = player
         self.grid = self.populate_grid(size)
-
 
     def populate_grid(self, size):
         """Populates an empty dictionary with coordinate tuples
@@ -33,20 +35,39 @@ class Map(object):
         if randint(0, 100) < (self.density*100):
             return choice(self.objects)
         else:
-            return ' '
+            return '-'
+
+    def get_objects(self):
+        obj_dic = {}
+        for e in self.grid:
+            if self.grid[e] != '-':
+                obj_dic[e] = self.grid[e]
+        return obj_dic
+
+    def get_player_inters(self):
+        pos = self.player.get_position()
+        objs = self.get_objects()
+        for e in objs.keys():
+            if pos == e:
+                return pos, objs[e]
+
+    def get_player_info(self):
+        return self.player
 
     def render_grid(self):
         """Renders the objects in the grid as a formatted string."""
-        grid = self.grid.keys()
-        grid_s = ""
-
+        grid_l = []
+        grid_s = ''
         count = 0
-        while count < self.size:
-            for i in grid:
-                if i.get_x() == count:
-                    grid_s += str(self.grid[i])
+
+        for e in self.grid:
+            grid_l += ((e, self.grid[e]),)
+
+        for e in sorted(grid_l):
+            grid_s += str(e) + ' '
             count += 1
-            grid_s += "\n"
+            if count % self.size == 0:
+                grid_s += "\n"
 
         return grid_s
 
@@ -56,21 +77,78 @@ class C(object):
         self.x = x
         self.y = y
 
+    def d_pos(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
     def get_x(self):
         return self.x
 
     def get_y(self):
         return self.y
 
+    def distance(self, other):
+        dx = other.get_x() - self.x
+        dy = other.get_y() - self.y
+        return sqrt(dx**2 + dy**2)
+
+    def theta(self, other):
+        dx = other.get_x() - self.x
+        dy = other.get_y() - self.y
+        return degrees(atan2(dy, dx))
+
+    def cardinality(self, other):
+        theta = self.theta(other)
+
+        if theta <= 22.5:
+            direction = "E"
+        if 22.5 < theta <= 67.5:
+            direction = "NE"
+        if 67.5 < theta <= 112.5:
+            direction = "N"
+        if 112.5 < theta <= 157.5:
+            direction = "NW"
+        if 157.5 < theta <= 202.5:
+            direction = "W"
+        if 202.5 < theta <= 247.5:
+            direction = "SW"
+        if 247.5 < theta <= 292.5:
+            direction = "S"
+        if 292.5 < theta <= 337.5:
+            direction = "SE"
+        if 337.5 < theta:
+            direction = "E"
+        return direction
+
     def __str__(self):
         return str((self.x, self.y))
+
+    def __eq__(self, other):
+        return ((self.get_x(), self.get_y()),) == ((other.get_x(), other.get_y()),)
 
     def __repr__(self):
         return str((self.x, self.y))
 
 
-random_objects = ['A', 'B', 'C', 'D', 'E']
-
+#
+random_objects = ['A', 'B', 'C', 'D']
+p = Player("P", 100, ["item"], C(0, 0))
 s = Map(20, random_objects, 0.05)
-
-print(s.render_grid())
+print(s.get_objects())
+#
+# print(s.get_player_inters())
+# print(s.get_player_info())
+#
+# p.change_position(0, 2)
+#
+# print(s.get_player_inters())
+# print(s.get_player_info())
+#
+# p.change_position(2, 0)
+#
+# print(s.get_player_inters())
+# print(s.get_player_info())
+#
+# grid = s.render_grid()
+#
+# print(grid)
